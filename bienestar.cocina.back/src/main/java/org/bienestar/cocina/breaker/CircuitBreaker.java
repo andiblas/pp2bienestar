5384940adc78b750f3b3a62fd2d6cc0d0bb0829e
Andres Blas Pujadas <andy_blas@hotmail.com>
2018-04-13 20:54:50 -0300

    cambio de cb

--- bienestar.cocina.back/src/main/java/org/bienestar/cocina/breaker/CircuitBreaker.java
+++ bienestar.cocina.back/src/main/java/org/bienestar/cocina/breaker/CircuitBreaker.java
@@ -1,87 +1,77 @@
 package org.bienestar.cocina.breaker;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class CircuitBreaker<T extends Sender> implements Runnable{
+import org.bienestar.cocina.excepciones.CircuitBreakerOpenException;
+import org.bienestar.cocina.mensajes.SendMessageCommand;
 
-	private String nombre;
-	private Sender sender;
+public class CircuitBreaker {
+
+	private CircuitBreakerCommand command;
 	private Integer errorLimit;
 	private Integer timeout;
 	private Integer errorCount;
-	private Queue<Message> colaMensajes = new ArrayDeque<Message>();
-	private String estado = "close";
-	
-	public CircuitBreaker(String nombre, Sender sender, Integer errorLimit, Integer timeout) {
+	private CircuitBreakerState state = CircuitBreakerState.CLOSE;
+
+	public CircuitBreaker(CircuitBreakerCommand command, Integer errorLimit,
+			Integer timeout) {
 		super();
-		this.nombre = nombre;
-		this.sender = sender;
+		this.command = command;
 		this.errorLimit = errorLimit;
 		this.timeout = timeout;
 		this.errorCount = 0;
 	}
 
-	public void run() {
-		System.out.println("entro hilo");
-		while(true){
-			if(!colaMensajes.isEmpty()){
-				Message message = colaMensajes.poll();
-				
-				try {
-					if("open".equals(estado)){
-						System.out.println("esta open");
-						addMessage(message);
-						continue;
-					}
-					
-					sender.send(message);
-					
-					if("half".equals(estado)){
-						estado = "close";
-						System.out.println("pongo estado close");
-						errorCount = 0;
-					}
-				} catch (Exception e) {
-					errorCount += 1;
-					System.out.println("error count: " + errorCount);
-					addMessage(message);
-					if(("close".equals(estado) && errorCount >= errorLimit) || "half".equals(estado)){
-						estado = "open";
-						System.out.println("pongo estado open");
-						TimerTask task = new TimerTask() {
-							
-							@Override
-							public void run() {
-								estado = "half";
-								System.out.println("pongo estado half");
-							}
-						};
-						Timer timer = new Timer();
-						timer.schedule(task, timeout);
-					}
-				}
-				
+	public void run() throws Exception {
+
+		try {
+			if (CircuitBreakerState.OPEN.equals(state))
+				throw new CircuitBreakerOpenException();
+
+			command.execute();
+
+			if (CircuitBreakerState.HALF_OPEN.equals(state)) {
+				changeState(CircuitBreakerState.CLOSE);
+				errorCount = 0;
 			}
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		} catch (CircuitBreakerOpenException e) {
+			throw e;
+		} catch (Exception e) {
+
+			if (this.errorCount < this.errorLimit)
+				this.errorCount++;
+
+			if (this.errorCount == this.errorLimit) {
+				trip();
+				startTimer();
 			}
+			throw e;
 		}
 	}
-	
-	public void addMessage(Message message){
-		colaMensajes.add(message);
-	}
-	
-	public String getEstado(){
-		return estado;
+
+	private void startTimer() {
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				changeState(CircuitBreakerState.HALF_OPEN);
+			}
+		};
+		Timer timer = new Timer();
+		timer.schedule(task, timeout);
 	}
 
-	public String getNombre() {
-		return nombre;
+	private void trip() {
+		changeState(CircuitBreakerState.OPEN);
 	}
+
+	private void changeState(CircuitBreakerState newState) {
+		this.state = newState;
+	}
+
+	public void setCommand(CircuitBreakerCommand command) {
+		this.command = command;
+	}
+
 }
+
