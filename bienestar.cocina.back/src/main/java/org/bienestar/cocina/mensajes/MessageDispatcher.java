e9143d467dd7aa4a9ed95681ff8033d8fa9b2be7
Andres Blas Pujadas <andy_blas@hotmail.com>
2018-04-13 21:12:15 -0300

    add MessageDispatcher

---
+++ bienestar.cocina.back/src/main/java/org/bienestar/cocina/mensajes/MessageDispatcher.java
@@ -0,0 +1,26 @@
+package org.bienestar.cocina.mensajes;
+
+import org.bienestar.cocina.breaker.CircuitBreaker;
+import org.bienestar.cocina.breaker.CircuitBreakerName;
+import org.bienestar.cocina.breaker.CircuitBreakerRegistry;
+import org.bienestar.cocina.breaker.EjemploSender;
+
+public class MessageDispatcher {
+
+	private String message;
+
+	public MessageDispatcher(String message) {
+		this.message = message;
+	}
+
+	public void dispatch() throws Exception {
+		CircuitBreakerRegistry registry = CircuitBreakerRegistry.getInstance();
+		CircuitBreaker cb = registry
+				.getCircuitBreaker(CircuitBreakerName.EJEMPLO.toString());
+		SendMessageCommand sender = new SendMessageCommand(new EjemploSender(),
+				message);
+		cb.setCommand(sender);
+		cb.run();
+	}
+
+}
