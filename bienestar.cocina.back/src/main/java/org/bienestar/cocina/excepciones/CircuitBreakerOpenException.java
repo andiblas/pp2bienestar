1a6351d1b3984d19ac6b297cc9c726da1a4c96a6
Andres Blas Pujadas <andy_blas@hotmail.com>
2018-04-13 21:11:17 -0300

    add CircuitBreakerOpenException

---
+++ bienestar.cocina.back/src/main/java/org/bienestar/cocina/excepciones/CircuitBreakerOpenException.java
@@ -0,0 +1,10 @@
+package org.bienestar.cocina.excepciones;
+
+public class CircuitBreakerOpenException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
