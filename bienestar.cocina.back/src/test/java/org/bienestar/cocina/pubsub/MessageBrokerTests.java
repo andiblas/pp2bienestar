b73834c09846a404ba65c809f260dc7ffdc06104
andiblas <andy_blas@hotmail.com>
2018-04-15 12:58:43 -0300

    test para el messagebroker

---
+++ bienestar.cocina.back/src/test/java/org/bienestar/cocina/pubsub/MessageBrokerTests.java
@@ -0,0 +1,28 @@
+package org.bienestar.cocina.pubsub;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MessageBrokerTests {
+
+	@Before
+	public void setUp() {
+	}
+	
+	@Test()
+	public void subscriptionTest()  {
+		MessageBroker.getInstance().subscribe("TOPIC", new Subscriber() {
+			public void onMessageReceived(Object message) {
+				Assert.assertEquals(new Integer(1), (Integer) message);
+			}
+		});
+		MessageBroker.getInstance().publish("TOPIC", new Integer(1));
+	}
+	
+	@Test()
+	public void publishMessageTest() {
+		
+	}
+	
+}
