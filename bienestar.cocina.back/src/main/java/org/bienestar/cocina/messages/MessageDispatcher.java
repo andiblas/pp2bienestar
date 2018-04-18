78c9ad7dfaaef9c7bba6f2b92860a117d57b4a2f
andiblas <andy_blas@hotmail.com>
2018-04-18 15:32:26 -0300

    refactor del message dispatcher + test

--- bienestar.cocina.back/src/main/java/org/bienestar/cocina/messages/MessageDispatcher.java
+++ bienestar.cocina.back/src/main/java/org/bienestar/cocina/messages/MessageDispatcher.java
@@ -9,19 +9,18 @@ import org.bienestar.cocina.exceptions.SendingException;
 public class MessageDispatcher {
 
 	private String message;
+	private CircuitBreaker cb;
+	private Sender sender;
 
-	public MessageDispatcher(String message) {
+	public MessageDispatcher(String message, CircuitBreaker cb, Sender sender) {
 		this.message = message;
+		this.cb = cb;
+		this.sender = sender;
 	}
 
 	public void dispatch() throws Exception {
-		CircuitBreakerRegistry registry = CircuitBreakerRegistry.getInstance();
-		CircuitBreaker cb = registry.getCircuitBreaker(CircuitBreakerName.EJEMPLO.toString());
-		SendMessageCommand sender = new SendMessageCommand(new Sender() {
-			public void send(String message) throws SendingException {
-			}
-		}, message);
-		cb.setCommand(sender);
+		SendMessageCommand sendCommand = new SendMessageCommand(sender, message);
+		cb.setCommand(sendCommand);
 		cb.run();
 	}
 
