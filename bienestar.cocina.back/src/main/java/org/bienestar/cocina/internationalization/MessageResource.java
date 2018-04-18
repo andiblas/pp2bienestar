65e22735d2549c7b47fdd5f9f771a0f6e69dfb19
Alejandro Herrera <alef.herrera@gmail.com>
2018-04-17 23:54:37 -0300

    agrego posibilidad de generar instancias para cada idioma

--- bienestar.cocina.back/src/main/java/org/bienestar/cocina/internationalization/MessageResource.java
+++ bienestar.cocina.back/src/main/java/org/bienestar/cocina/internationalization/MessageResource.java
@@ -1,30 +1,44 @@
 package org.bienestar.cocina.internationalization;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class MessageResource {
 
-	private static MessageResource instance;
-	
 	private ResourceBundle bundle;
-	
+
+	private static Map<Locale, MessageResource> dic = new HashMap<Locale, MessageResource>();
+
 	private MessageResource(ResourceBundle bundle) {
 		this.bundle = bundle;
 	}
-	
-	public static MessageResource getInstance() {
-		if(instance == null) {
-			instance = new MessageResource(ResourceBundle.getBundle("messages"));
-		}
-		return instance;
+
+	private static MessageResource createInstance(Locale locale) {
+		return new MessageResource(ResourceBundle.getBundle("messages", locale));
 	}
-	
+
+	public static MessageResource getInstance() {
+		return getInstance(Locale.getDefault());
+	}
+
+	public static MessageResource getInstance(Locale locale) {
+		if (dic.containsKey(locale)) {
+			return dic.get(locale);
+		} else {
+			MessageResource res = createInstance(locale);
+			dic.put(locale, res);
+			return res;
+		}
+	}
+
 	public String getMessage(String key) {
 		String message;
 		try {
 			message = bundle.getString(key);
-			
+
 		} catch (MissingResourceException e) {
 			message = "";
 		}
