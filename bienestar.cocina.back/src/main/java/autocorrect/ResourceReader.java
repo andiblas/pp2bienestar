ac4fa32c7822699016dd5fa2365a1b9e00c830f2
Alejandro Herrera <alef.herrera@gmail.com>
2018-04-21 12:47:20 -0300

    renombro clases y empiezo a separa responsabilidades

---
+++ bienestar.cocina.back/src/main/java/autocorrect/ResourceReader.java
@@ -0,0 +1,46 @@
+package autocorrect;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ResourceReader {
+
+	private final Map<Character, List<String>> nWords = new HashMap<Character, List<String>>();
+	
+	public ResourceReader(String path) throws IOException {
+		super();
+		BufferedReader in = new BufferedReader(new FileReader(path));
+		Pattern p = Pattern.compile("\\w+");
+		for (String temp = ""; temp != null; temp = in.readLine()) {
+			Matcher m = p.matcher(temp.toLowerCase());
+			while (m.find()) {
+				String word = m.group();
+				for (Character c : word.toCharArray()) {
+					List<String> lista;
+					if (getWords().containsKey(c)) {
+						lista = getWords().get(c);
+					} else {
+						lista = new ArrayList<String>();
+					}
+					lista.add(word);
+					getWords().put(c, lista);
+				}
+			}
+		}
+		in.close();
+	}
+
+	public Map<Character, List<String>> getWords() {
+		return nWords;
+	}
+	
+	
+}
---
+++ bienestar.cocina.back/src/main/java/autocorrect/SpellFixer.java
@@ -0,0 +1,60 @@
+package autocorrect;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class SpellFixer {
+
+	private Map<Character, List<String>> repository;
+	private Comparator<Entry<String, Integer>> comparator;
+
+	public SpellFixer(Map<Character, List<String>> repository) throws IOException {
+		this.repository = repository;
+		comparator = (entry1, entry2) ->
+			entry1.getValue() > entry2.getValue()
+				|| (entry1.getValue() == entry2.getValue() && entry1.getKey().length() < entry2.getKey().length())
+				? 1
+				: -1;
+	}
+
+	public String sugerencia(String keyword) {
+		Map<String, Integer> candidates = new HashMap<String, Integer>();
+		for (Character c : keyword.toCharArray()) {
+			if (repository.containsKey(c)) {
+				this.contarOcurrencias(candidates, repository.get(c));
+			}
+		}
+		if (!candidates.isEmpty()) {		
+			return candidates.entrySet().stream().max(comparator)
+					.get().getKey();
+		}
+		return "";
+	}
+
+	private void contarOcurrencias(Map<String, Integer> candidates, List<String> list) {
+		for (String word : list) {
+			candidates.put(word, candidates.containsKey(word) ? candidates.get(word) + 1 : 1);
+		}
+	}
+
+	public static void main(String args[]) throws IOException {
+		String input = "poio";
+		String input2 = "poshio";
+		String input3 = "poyo";
+		ResourceReader reader = new ResourceReader("E:\\git\\pp2bienestar\\bienestar.cocina.back\\src\\main\\java\\autocorrect\\big.txt");
+		SpellFixer spelling = new SpellFixer(reader.getWords());
+		System.out.println("Input: " + input);
+		System.out.println("Sugerencia: " + spelling.sugerencia(input));
+		System.out.println("Input: " + input2);
+		System.out.println("Sugerencia: " + spelling.sugerencia(input2));
+		System.out.println("Input: " + input3);
+		System.out.println("Sugerencia: " + spelling.sugerencia(input3));
+		System.out.println("Input: " + "cala");
+		System.out.println("Sugerencia: " + "gato");
+	}
+
+}
