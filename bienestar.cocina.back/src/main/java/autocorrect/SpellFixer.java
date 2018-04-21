9ee32bed4cf3ea06e45799f29e8c613e96a2ff8f
Alejandro Herrera <alef.herrera@gmail.com>
2018-04-21 13:09:34 -0300

    saco dependencia de comparador y renombro metodos

--- bienestar.cocina.back/src/main/java/autocorrect/SpellFixer.java
+++ bienestar.cocina.back/src/main/java/autocorrect/SpellFixer.java
@@ -12,34 +12,26 @@ public class SpellFixer {
 	private Map<Character, List<String>> repository;
 	private Comparator<Entry<String, Integer>> comparator;
 
-	public SpellFixer(Map<Character, List<String>> repository) throws IOException {
+	public SpellFixer(Map<Character, List<String>> repository, Comparator<Entry<String, Integer>> comparator)
+			throws IOException {
 		this.repository = repository;
-		comparator = new Comparator<Entry<String, Integer>>() {
-			@Override
-			public int compare(Entry<String, Integer> entry1, Entry<String, Integer> entry2) {
-				return entry1.getValue() > entry2.getValue()
-					|| (entry1.getValue() == entry2.getValue() && entry1.getKey().length() < entry2.getKey().length())
-					? 1
-					: -1;
-			}
-		};
+		this.comparator = comparator;
 	}
 
-	public String sugerencia(String keyword) {
+	public String getBestFit(String keyword) {
 		Map<String, Integer> candidates = new HashMap<String, Integer>();
 		for (Character c : keyword.toCharArray()) {
 			if (repository.containsKey(c)) {
-				this.contarOcurrencias(candidates, repository.get(c));
+				this.fillCandidates(candidates, repository.get(c));
 			}
 		}
-		if (!candidates.isEmpty()) {		
-			return candidates.entrySet().stream().max(comparator)
-					.get().getKey();
+		if (!candidates.isEmpty()) {
+			return candidates.entrySet().stream().max(comparator).get().getKey();
 		}
 		return "";
 	}
 
-	private void contarOcurrencias(Map<String, Integer> candidates, List<String> list) {
+	private void fillCandidates(Map<String, Integer> candidates, List<String> list) {
 		for (String word : list) {
 			candidates.put(word, candidates.containsKey(word) ? candidates.get(word) + 1 : 1);
 		}
@@ -49,14 +41,22 @@ public class SpellFixer {
 		String input = "poio";
 		String input2 = "poshio";
 		String input3 = "poyo";
-		ResourceReader reader = new ResourceReader("E:\\git\\pp2bienestar\\bienestar.cocina.back\\src\\main\\java\\autocorrect\\big.txt");
-		SpellFixer spelling = new SpellFixer(reader.getWords());
+		Comparator<Entry<String, Integer>> comparator = new Comparator<Entry<String, Integer>>() {
+			@Override
+			public int compare(Entry<String, Integer> entry1, Entry<String, Integer> entry2) {
+				return entry1.getValue() > entry2.getValue() || (entry1.getValue() == entry2.getValue()
+						&& entry1.getKey().length() < entry2.getKey().length()) ? 1 : -1;
+			}
+		};
+		ResourceReader reader = new ResourceReader(
+				"E:\\git\\pp2bienestar\\bienestar.cocina.back\\src\\main\\java\\autocorrect\\big.txt");
+		SpellFixer spelling = new SpellFixer(reader.getWords(), comparator);
 		System.out.println("Input: " + input);
-		System.out.println("Sugerencia: " + spelling.sugerencia(input));
+		System.out.println("Sugerencia: " + spelling.getBestFit(input));
 		System.out.println("Input: " + input2);
-		System.out.println("Sugerencia: " + spelling.sugerencia(input2));
+		System.out.println("Sugerencia: " + spelling.getBestFit(input2));
 		System.out.println("Input: " + input3);
-		System.out.println("Sugerencia: " + spelling.sugerencia(input3));
+		System.out.println("Sugerencia: " + spelling.getBestFit(input3));
 		System.out.println("Input: " + "cala");
 		System.out.println("Sugerencia: " + "gato");
 	}
