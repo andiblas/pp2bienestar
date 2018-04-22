f538d9ce1133fda69b759ce57dc65a151a8a8c42
Alejandro Herrera <alef.herrera@gmail.com>
2018-04-22 18:12:24 -0300

    clases, test y multilenguaje de consumisiones

--- bienestar.cocina.back/src/main/java/org/bienestar/cocina/verifier/ConsumptionRepository.java
+++ bienestar.cocina.back/src/main/java/org/bienestar/cocina/verifier/ConsumptionRepository.java
@@ -2,19 +2,18 @@ package org.bienestar.cocina.verifier;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map.Entry;
 
 import org.bienestar.cocina.domain.Consumption;
 
 public class ConsumptionRepository {
 
-	private List<Entry<Consumption, Integer>> consumptions;
+	private List<Consumption> consumptions;
 
 	public ConsumptionRepository() {
-		consumptions = new ArrayList<Entry<Consumption,Integer>>();
+		consumptions = new ArrayList<Consumption>();
 	}
 
-	public List<Entry<Consumption, Integer>> getConsumptions() {
+	public List<Consumption> getConsumptions() {
 		return consumptions;
 	}
 }
--- bienestar.cocina.back/src/main/java/org/bienestar/cocina/verifier/ConsumptionVerifier.java
+++ bienestar.cocina.back/src/main/java/org/bienestar/cocina/verifier/ConsumptionVerifier.java
@@ -1,9 +1,9 @@
 package org.bienestar.cocina.verifier;
 
-import java.util.Map.Entry;
 import java.util.OptionalDouble;
 
 import org.bienestar.cocina.domain.Consumption;
+import org.bienestar.cocina.exceptions.ConsumptionOutOfRangeException;
 
 public class ConsumptionVerifier {
 
@@ -14,34 +14,23 @@ public class ConsumptionVerifier {
 
 	}
 
-	public void RegisterConsumption(Consumption consumption, Integer commensal) throws Exception {
-		OptionalDouble average = consumptionRepository.getConsumptions().stream().filter(x -> x.getKey() == consumption)
-				.mapToInt(x -> x.getValue()).average();
+	public void RegisterConsumption(Consumption consumption, Integer commensal) throws ConsumptionOutOfRangeException {
+		OptionalDouble average = consumptionRepository.getConsumptions().stream()
+				.filter(x -> x.getIngredient() == consumption.getIngredient()).mapToDouble(x -> x.getQuantity())
+				.average();
 		if (average.isPresent()) {
 			Double min, max;
 			Double value = average.getAsDouble();
 			Double tolerance = value * 0.20;
 			min = value - tolerance;
 			max = value + tolerance;
-			if (commensal < min || commensal > max) {
-				throw new Exception("prueba");
+			Double unitConsumption = consumption.getQuantity() / commensal;
+			if (unitConsumption < min || unitConsumption > max) {
+				throw new ConsumptionOutOfRangeException();
 			}
 		}
-		
-		consumptionRepository.getConsumptions().add(new Entry<Consumption, Integer>() {
 
-			public Integer setValue(Integer value) {
-				return null;
-			}
-
-			public Integer getValue() {
-				return commensal;
-			}
-
-			public Consumption getKey() {
-				return consumption;
-			}
-		});
+		consumptionRepository.getConsumptions().add(consumption);
 	}
 
 }
