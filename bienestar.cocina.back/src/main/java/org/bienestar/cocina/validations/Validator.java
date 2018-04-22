5ad3ad8cf1ee3dc0a8228a441722004fb0b5fc15
leangonz <leo-2590@hotmail.com>
2018-04-22 20:53:03 -0300

    se saca validador obsoleto

--- bienestar.cocina.back/src/main/java/org/bienestar/cocina/validations/Validator.java
+++
@@ -1,8 +0,0 @@
-package org.bienestar.cocina.validations;
-
-import org.bienestar.cocina.exceptions.ValidacionException;
-
-public interface Validator {
-
-	void execute(Object param) throws ValidacionException;
-}
--- bienestar.cocina.front/src/main/java/org/bienestar/cocina/controller/PlanillaDiariaController.java
+++ bienestar.cocina.front/src/main/java/org/bienestar/cocina/controller/PlanillaDiariaController.java
@@ -3,25 +3,19 @@ package org.bienestar.cocina.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import org.bienestar.cocina.validations.Validador;
-import org.bienestar.cocina.validations.impl.DateValidator;
 import org.bienestar.cocina.view.PlantillaDiariaView;
 
 public class PlanillaDiariaController extends Controller<PlantillaDiariaView>{
 	
-	private Validador fechaValidador;
 
 	public PlanillaDiariaController(PlantillaDiariaView view) {
 		super(view);
-		fechaValidador = new DateValidator();
-		fechaValidador.addObserver(view);
 		view.asociarEnviarListener(new EnviarListener());
 	}
 	
 	class EnviarListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
-			fechaValidador.validar(getView().getTxtFecha().getText());
 		}
     	
     }
