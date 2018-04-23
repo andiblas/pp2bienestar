e5fb79dc582c91e109f2bf0435bf8c0066f4952d
Alejandro Herrera <alef.herrera@gmail.com>
2018-04-22 23:11:36 -0300

    validaciones para ingreso de cantidad de comensales

--- bienestar.cocina.back/src/test/java/org/bienestar/cocina/diners/DinersTest.java
+++ bienestar.cocina.back/src/test/java/org/bienestar/cocina/diners/DinersTest.java
@@ -2,16 +2,38 @@ package org.bienestar.cocina.diners;
 
 import static org.junit.Assert.assertEquals;
 
+import org.bienestar.cocina.exceptions.InvalidQuantityException;
 import org.junit.Test;
 
 public class DinersTest {
 
 	@Test
-	public void dinersQuantity() {
+	public void dinersQuantity() throws InvalidQuantityException {
 		DinersRepository dinerRepository = new DinersRepository();
 		DinerInput dinersInput = new DinerInput(dinerRepository);
-		dinersInput.regiterQuantity(10);
+		dinersInput.regiterQuantity("10");
 		assertEquals(new Integer(10), dinersInput.getDinersQuantity());
+	}
+	
+	@Test(expected=InvalidQuantityException.class)
+	public void dinersQuantityA() throws InvalidQuantityException {
+		DinersRepository dinerRepository = new DinersRepository();
+		DinerInput dinersInput = new DinerInput(dinerRepository);
+		dinersInput.regiterQuantity("A");
+	}
+	
+	@Test(expected=InvalidQuantityException.class)
+	public void dinersQuantityNegative() throws InvalidQuantityException {
+		DinersRepository dinerRepository = new DinersRepository();
+		DinerInput dinersInput = new DinerInput(dinerRepository);
+		dinersInput.regiterQuantity("-5");
+	}
+	
+	@Test(expected=InvalidQuantityException.class)
+	public void dinersQuantityDecimal() throws InvalidQuantityException {
+		DinersRepository dinerRepository = new DinersRepository();
+		DinerInput dinersInput = new DinerInput(dinerRepository);
+		dinersInput.regiterQuantity("10.5");
 	}
 
 	// Si se ingresa la letra ‘A’ o ‘a’ como cantidad de comensales, el sistema
