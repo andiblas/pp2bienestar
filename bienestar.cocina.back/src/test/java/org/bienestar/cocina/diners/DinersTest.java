d5dd3e4217c37b7c8c064c9197f4f58eb46dc6dd
Alejandro Herrera <alef.herrera@gmail.com>
2018-04-22 23:16:53 -0300

    saco comentarios de mas

--- bienestar.cocina.back/src/test/java/org/bienestar/cocina/diners/DinersTest.java
+++ bienestar.cocina.back/src/test/java/org/bienestar/cocina/diners/DinersTest.java
@@ -35,15 +35,4 @@ public class DinersTest {
 		DinerInput dinersInput = new DinerInput(dinerRepository);
 		dinersInput.regiterQuantity("10.5");
 	}
-
-	// Si se ingresa la letra ‘A’ o ‘a’ como cantidad de comensales, el sistema
-	// arrojará una excepción de cantidad de comensales inválidos
-
-	// En el caso de ingresar un número negativo, deberá dar error. Por ejemplo,
-	// si se ingresa ‘-5’ como cantidad de comensales, el sistema deberá dar
-	// error.
-
-	// Si se ingresa 10,5 como cantidad de comensales, el sistema arrojará una
-	// excepción de cantidad de comensales inválidos.
-
 }
