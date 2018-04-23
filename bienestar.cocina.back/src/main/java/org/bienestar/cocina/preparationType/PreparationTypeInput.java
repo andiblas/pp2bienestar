0cb1a8d1c4ad0171aaec8be5ffdfc844a255b4d5
VANE\VaneD <vanesa.dougan@hotmail.com>
2018-04-22 22:32:11 -0300

    PreparationType User Story 5

---
+++ bienestar.cocina.back/src/main/java/org/bienestar/cocina/preparationType/PreparationTypeInput.java
@@ -0,0 +1,29 @@
+package org.bienestar.cocina.preparationType;
+
+import org.bienestar.cocina.diners.DinersRepository;
+import org.bienestar.cocina.domain.Diner;
+import org.bienestar.cocina.domain.PreparationType;
+
+public class PreparationTypeInput {
+
+	private PreparationTypeRepository preparationTypeRepository;
+	public PreparationTypeInput(PreparationTypeRepository preparationTypeRepository)
+	{
+		this.preparationTypeRepository=preparationTypeRepository;
+	}
+	
+	public void registerType(String type)
+	{
+		PreparationType preparationType=new PreparationType();
+		preparationType.setType(type);
+		preparationTypeRepository.getPreparationTypes().add(preparationType);
+	}
+	
+	public String getPreparationType()
+	{
+		return preparationTypeRepository.getPreparationTypes().toString();
+	}
+	
+	
+	
+}
