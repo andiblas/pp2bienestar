9d8f569e40758aa740979909a2868350e3e81bf3
Alejandro Herrera <alef.herrera@gmail.com>
2018-05-17 20:29:30 -0300

    agrego clases de us13

---
+++ bienestar.cocina.back.messenger/src/main/java/bienestar/cocina/back/messenger/MessageDispatcher.java
@@ -0,0 +1,9 @@
+package bienestar.cocina.back.messenger;
+
+public class MessageDispatcher {
+
+	public MessageDispatcher(Message msg) {
+		//TOOD: meterle logica para que envie a un archivo/socket, lo que sea
+	}
+	
+}
