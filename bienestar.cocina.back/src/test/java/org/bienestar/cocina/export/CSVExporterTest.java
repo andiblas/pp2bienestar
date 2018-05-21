b3fc441b9922b54070c27d5d40a0919162cd90a2
VANE\VaneD <vanesa.dougan@hotmail.com>
2018-05-20 21:53:28 -0300

    CSVExporterTest

---
+++ bienestar.cocina.back/src/test/java/org/bienestar/cocina/export/CSVExporterTest.java
@@ -0,0 +1,14 @@
+package org.bienestar.cocina.export;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CSVExporterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
