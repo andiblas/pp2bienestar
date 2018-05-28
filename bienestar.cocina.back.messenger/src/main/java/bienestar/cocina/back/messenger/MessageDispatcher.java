f1ea43e6f1ffad32d72e6fdd5ee50bcc8b6a8454
Alejandro <alef.herrera@gmail.com>
2018-05-28 19:00:22 -0300

    test

--- bienestar.cocina.back.messenger/src/main/java/bienestar/cocina/back/messenger/MessageDispatcher.java
+++
@@ -1,9 +0,0 @@
-package bienestar.cocina.back.messenger;
-
-public class MessageDispatcher {
-
-	public MessageDispatcher(Message msg) {
-		//TOOD: meterle logica para que envie a un archivo/socket, lo que sea
-	}
-	
-}
---
+++ bienestar.cocina.back.messenger/src/test/java/org/bienestar/cocina/back/messenger/RetryManagerTest.java
@@ -0,0 +1,14 @@
+package org.bienestar.cocina.back.messenger;
+
+import org.bienestar.cocina.back.messenger.RetryManager;
+import org.junit.Test;
+
+public class RetryManagerTest {
+
+	@Test
+	public void asd() {
+		RetryManager retryManager = new RetryManager();
+		
+	}
+	
+}
