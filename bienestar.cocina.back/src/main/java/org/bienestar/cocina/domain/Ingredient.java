c6582bc7c66fa28d2abc646b1bafa3620725e080
andiblas <andy_blas@hotmail.com>
2018-06-01 01:26:14 -0300

    equals para Ingrediente y Preparacion

--- bienestar.cocina.back/src/main/java/org/bienestar/cocina/domain/Ingredient.java
+++ bienestar.cocina.back/src/main/java/org/bienestar/cocina/domain/Ingredient.java
@@ -8,10 +8,37 @@ public class Ingredient {
 
 	public Ingredient() {
 	}
-	
-	public Ingredient(String name,MeasureType measureType) {
+
+	public Ingredient(String name, MeasureType measureType) {
 		this.name = name;
 		this.measureType = measureType;
+	}
+
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ingredient other = (Ingredient) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 	public String getName() {
